d6244df68b5c2d972a620a8362061fc5afbb9423
Tory Herman <tory.herman12@gmail.com>
2017-08-21 19:15:33 -0500

    add rerun dependency

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ $LOAD_PATH << File.join(File.dirname(__FILE__),
                         "app",
                         "controllers")
 
-require "quotes_controller"
-
 module BestQuotes
   class Application < Rulers::Application
   end
